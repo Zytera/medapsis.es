1f91d5aa09a630ea55a0113da454b737ee73c4a6
César M. Cristóbal <cesar@callepuzzle.com>
2025-04-11 13:45:12 +0200

    wip layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import partytown from '@astrojs/partytown';
+import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import flowbiteReact from "flowbite-react/plugin/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), partytown()],
+  integrations: [
+    react({
+      experimentalReactChildren: true,
+    }),
+    partytown(),
+    flowbiteReact(),
+  ],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
